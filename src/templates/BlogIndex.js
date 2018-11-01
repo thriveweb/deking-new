f19ccde0c00d052f30e33ececc1a88b7d8d838da
Vincent Weltje <vincent@thriveweb.com.au>
2018-11-01 11:29:20 +1000

    changed blog post order

--- src/templates/BlogIndex.js
+++ src/templates/BlogIndex.js
@@ -80,7 +80,7 @@ export const pageQuery = graphql`
 
     posts: allMarkdownRemark(
       filter: { fields: { contentType: { eq: "posts" } } }
-      sort: { order: DESC, fields: [frontmatter___date] }
+      sort: { order: ASC, fields: [frontmatter___date] }
     ) {
       edges {
         node {
