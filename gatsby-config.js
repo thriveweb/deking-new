aa6392d096d9e01a65e875a23ed0b6d7e72ec1a6
Alex <alex@thriveweb.com.au>
2019-01-22 13:53:54 +1000

    add offline support

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-transformer-yaml',
+    'gatsby-plugin-offline',
 
     // Add static assets before markdown files
     {
