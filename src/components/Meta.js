4382d3172cc45e98673b13d6b0071c9c1886f279
Alex <alex@thriveweb.com.au>
2019-01-24 12:01:42 +1000

    update meta for meta scripts

--- src/components/Meta.js
+++ src/components/Meta.js
@@ -1,54 +1,67 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Helmet from 'react-helmet'
-import _get from 'lodash/get'
-import _pickBy from 'lodash/pickBy'
 
-const onlyTruthyValues = obj => _pickBy(obj, item => item)
-
-const Meta = props => {
-  const {
-    title,
-    url,
-    description,
-    absoluteImageUrl = '',
-    headerScripts,
-    noindex,
-    canonicalLink,
-    siteTitle,
-    siteDescription
-    // overwrite { title, description } if in fields or fields.meta
-  } = { ...props }
-
-  // write headerScripts
-  if (typeof window !== 'undefined') {
-    const headerScriptsElement = document.head.querySelector('#headerScripts')
-    if (headerScripts && headerScriptsElement) {
-      headerScriptsElement.outerHTML = headerScripts
+class Meta extends React.Component {
+  componentDidMount() {
+    const { headerScripts } = this.props
+    // write headerScripts
+    if (typeof window !== 'undefined') {
+      const intVal = setInterval(() => {
+        let headerScriptsElement = false
+        try {
+          headerScriptsElement = document.getElementById('headerScripts')
+        } catch (err) {
+          return
+        }
+        if (headerScripts && headerScriptsElement) {
+          headerScriptsElement.outerHTML = headerScripts
+          clearInterval(intVal)
+        }
+      }, 100)
     }
   }
-
-  return (
-    <Helmet>
-      {title && <title>{title}</title>}
-      {title && <meta property="og:title" content={title} />}
-      {description && <meta name="description" content={description} />}
-      {description && <meta property="og:description" content={description} />}
-      {url && <meta property="og:type" content="website" />}
-      {url && <meta property="og:url" content={url} />}
-      {noindex && <meta name="robots" content="noindex" />}
-      {canonicalLink && <link rel="canonical" href={canonicalLink} />}
-      <meta property="og:locale" content="en_US" />
-      <meta property="og:site_name" content={siteTitle} />
-      <meta name="twitter:description" content={siteDescription} />
-      <meta name="twitter:title" content={siteTitle} />
-      <meta name="twitter:image" content={absoluteImageUrl} />
-      <meta property="og:image:secure_url" content={absoluteImageUrl} />
-      <meta property="og:image" content={absoluteImageUrl} />
-      <meta name="twitter:card" content={absoluteImageUrl} />
-    </Helmet>
-  )
+  render() {
+    const {
+      title,
+      url,
+      description,
+      absoluteImageUrl = '',
+      headerScripts,
+      noindex,
+      canonicalLink,
+      siteTitle,
+      siteDescription
+      // overwrite { title, description } if in fields or fields.meta
+    } = this.props
+    return (
+      <Helmet>
+        {title && <title>{title}</title>}
+        {title && <meta property="og:title" content={title} />}
+        {description && <meta name="description" content={description} />}
+        {description && (
+          <meta property="og:description" content={description} />
+        )}
+        {url && <meta property="og:type" content="website" />}
+        {url && <meta property="og:url" content={url} />}
+        {noindex && <meta name="robots" content="noindex" />}
+        {canonicalLink && <link rel="canonical" href={canonicalLink} />}
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:site_name" content={siteTitle} />
+        <meta name="twitter:description" content={siteDescription} />
+        <meta name="twitter:title" content={siteTitle} />
+        <meta name="twitter:image" content={absoluteImageUrl} />
+        <meta property="og:image:secure_url" content={absoluteImageUrl} />
+        <meta property="og:image" content={absoluteImageUrl} />
+        <meta name="twitter:card" content={absoluteImageUrl} />
+        <meta
+          name="headerScriptsElement"
+          id="headerScripts"
+          content="replace headerScriptsElement"
+        />
+      </Helmet>
+    )
+  }
 }
-
 export default Meta
 
 export const query = graphql`
