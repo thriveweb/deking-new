2f1589bff551e54d140b20b7e81fe13182b70e14
Harshna Goratela <harshna.goratela@gmail.com>
2023-09-24 18:44:08 +0530

    Fix double up in the menu and remove load more button to show all items

--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import _sortBy from 'lodash/sortBy'
+import _filter from 'lodash/filter'
 
 import ServiceCard from '../components/ServiceCard'
 import './ServicesSection.css'
@@ -9,13 +10,13 @@ class ServicesSection extends React.Component {
     services: [],
     title: '',
     limit: 12,
-    showLoadMore: true,
+    showLoadMore: false, // disable loadmore button
     loadMoreTitle: 'Load More',
     perPageLimit: 12
   }
 
   state = {
-    limit: this.props.limit
+    // limit: this.props.limit //this line is commented to disable loadmore button
   }
 
   increaseLimit = () => {
@@ -28,8 +29,9 @@ class ServicesSection extends React.Component {
     const { services, title, showLoadMore, loadMoreTitle } = this.props
     const { limit } = this.state
 
-    const visibleServices = _sortBy(services, ['date'])
+    let visibleServices = _sortBy(services, ['date'])
       .reverse()
+      .filter(page => page.fields.slug != '/services/decks/' && page.fields.slug != '/services/home-modifications/')
       // show all unlesss you set a limit.
       .slice(0, limit || services.length)
 
