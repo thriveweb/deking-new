5b0840747dd11f13ab3342f8567807d57d85db1f
Kurtis <36742865+kurtisbradley@users.noreply.github.com>
2024-05-17 10:15:01 +1000

    Insert GTM

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -29,6 +29,14 @@ export default ({ children, data }) => {
         />
 
         <script>
+            {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5G5GGG3T');`}
+        </script>
+
+        <script>
           {`!function(f,b,e,v,n,t,s)
           {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
           n.callMethod.apply(n,arguments):n.queue.push(arguments)};
